77a4094b653e71297cfb53573c783cff1fa22dc7
nostroguardian <shmelivan5@yandex.ru>
2024-12-12 18:15:53 +0300

    fix

--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -4,7 +4,7 @@ import { Stack, Typography } from '@mui/material';
 export const Home = () => {
 	return (
 		<>
-			<Stack spacing={2} alignItems="center">
+			<Stack spacing={2} alignItems="center" padding={2}>
 				<Typography variant="h4" color="textSecondary">
 					Home page
 				</Typography>
