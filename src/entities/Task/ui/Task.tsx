eef87e24b7ccfb93ba9d27c3443ef8549bb83a67
nostroguardian <shmelivan5@yandex.ru>
2024-12-12 18:05:18 +0300

    feat: simple preview

--- src/entities/Task/ui/Task.tsx
+++ src/entities/Task/ui/Task.tsx
@@ -3,8 +3,15 @@ import { Card, CardActions, CardContent, IconButton, Stack, Typography } from '@
 import PersonIcon from '@mui/icons-material/Person';
 import DoneAllIcon from '@mui/icons-material/DoneAll';
 import RemoveDoneIcon from '@mui/icons-material/RemoveDone';
+import { useState } from 'react';
 
 export const Task = ({ userId, id, title, completed }: ITodoTask) => {
+	const [completeStatus, setCompleteStatus] = useState(completed);
+
+	const updateCompleteStatusHandler = () => {
+		setCompleteStatus((state) => !state);
+	};
+
 	return (
 		<Card variant="outlined">
 			<CardContent>
@@ -23,12 +30,22 @@ export const Task = ({ userId, id, title, completed }: ITodoTask) => {
 				</Stack>
 			</CardContent>
 			<CardActions>
-				{completed ? (
-					<IconButton size="small" aria-label="uncomplete" color="secondary">
+				{completeStatus ? (
+					<IconButton
+						onClick={updateCompleteStatusHandler}
+						size="small"
+						aria-label="uncomplete"
+						color="secondary"
+					>
 						<RemoveDoneIcon />
 					</IconButton>
 				) : (
-					<IconButton size="small" aria-label="complete" color="primary">
+					<IconButton
+						onClick={updateCompleteStatusHandler}
+						size="small"
+						aria-label="complete"
+						color="primary"
+					>
 						<DoneAllIcon />
 					</IconButton>
 				)}
--- src/shared/api/todos/todos.ts
+++ src/shared/api/todos/todos.ts
@@ -1,8 +1,13 @@
 import axios from 'axios';
 import { ITodoTask } from '../../model';
 
-const baseUrl = 'https://jsonplaceholder.typicode.com/todos/';
+const baseUrl = 'https://jsonplaceholder.typicode.com/todos';
+// const baseUrl = 'https://retoolapi.dev/jAsG7g/data';
 
 export const getTodoTasks = (): Promise<ITodoTask[]> => {
 	return axios.get<ITodoTask[]>(baseUrl).then((res) => res.data);
+};
+
+export const updateTodoTask = (task: ITodoTask): Promise<ITodoTask> => {
+	return axios.put<ITodoTask>(`${baseUrl}/${task.id}`, task).then((res) => res.data);
 };
